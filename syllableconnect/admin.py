dff65469f7154aecbc7e6dbbff5d6f6c2165b6d2
kevin-crucible <kevin@crucible.care>
2025-06-03 08:40:03 +0800

    adjust admin panel

--- syllableconnect/admin.py
+++ syllableconnect/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(WordBank)
 admin.site.register(SyllableBank)
 # Register your models here.
 
+
+@admin.register(WordBank)
+class WordBankAdmin(admin.ModelAdmin):
+    list_display = ['word']  # Shows word column in the list view
+    search_fields = ['word']  # Adds search functionality
--- syllableconnect/models.py
+++ syllableconnect/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
-#
-# class WordBank(models.Model):
-#     word = models.CharField(max_length=50, unique=True)
-#     jp_word = models.CharField(max_length=20, null=True)
-#     syllable_count = models.IntegerField()
-#     level = models.IntegerField()
-#     connect_app = models.BooleanField(null=False, default=False)
-#
-#
-# class SyllableBank(models.Model):
-#     word = models.ForeignKey("WordBank", to_field="word", db_column="word", on_delete=models.CASCADE)
-#     syllables = ArrayField(models.CharField(max_length=19, blank=False))
-#
-
 
 class WordBank(models.Model):
     word = models.CharField(max_length=50, unique=True)
@@ -24,12 +10,7 @@ class WordBank(models.Model):
     connect_app = models.BooleanField(null=False, default=False)
 
     def __str__(self):
-        try:
-            if self.jp_word:
-                return f"{self.word} ({self.jp_word}) - Level {self.level}"
-            return f"{self.word} - Level {self.level}"
-        except:
-            return f"WordBank #{self.pk}"
+        return self.word
 
     class Meta:
         verbose_name = "Word"
@@ -39,17 +20,3 @@ class WordBank(models.Model):
 class SyllableBank(models.Model):
     word = models.ForeignKey("WordBank", to_field="word", db_column="word", on_delete=models.CASCADE)
     syllables = ArrayField(models.CharField(max_length=19, blank=False))
-
-    def __str__(self):
-        try:
-            word_display = str(self.word) if self.word else "Unknown word"
-            if self.syllables and len(self.syllables) > 0:
-                syllables_str = "-".join(str(s) for s in self.syllables if s)
-                return f"{word_display}: {syllables_str}"
-            return f"{word_display}: No syllables"
-        except:
-            return f"SyllableBank #{self.pk}"
-
-    class Meta:
-        verbose_name = "Syllable Breakdown"
-        verbose_name_plural = "Syllable Breakdowns"
